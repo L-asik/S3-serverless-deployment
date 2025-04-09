a51dac43ec300e8d7400cb18ba3d25b9fba33634
Antoni Lasik <101575224+L-asik@users.noreply.github.com>
2025-04-09 18:18:50 +0200

    Fixes and status messages

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,7 @@
 FROM public.ecr.aws/lambda/python:3.9
 COPY requirements.txt .
-RUN pip3 install -r requirements.txt --target "${LAMBDA_TASK_ROOT}"
-COPY model_download.py ${LAMBDA_TASK_ROOT}
-RUN python ${LAMBDA_TASK_ROOT}/model_download.py
-
-ENV HF_MODEL_DIR=/var/rsc
+RUN pip3 install --no-cache-dir --extra-index-url https://download.pytorch.org/whl/cpu -r requirements.txt --target "${LAMBDA_TASK_ROOT}"
+COPY pretrained_download.py ${LAMBDA_TASK_ROOT}
+RUN python ${LAMBDA_TASK_ROOT}/pretrained_download.py
 COPY handler.py ${LAMBDA_TASK_ROOT}
 CMD ["handler.lambda_handler"]
--- handler.py
+++ handler.py
@@ -6,12 +6,28 @@ from io import BytesIO
 import torch
 from transformers import AutoImageProcessor, AutoModelForImageClassification
 
-model_dir = os.environ.get('HF_MODEL_DIR', '/model')
+processor = AutoImageProcessor.from_pretrained("rsc/model", local_files_only=True)
+model = AutoModelForImageClassification.from_pretrained("rsc/model", local_files_only=True)
 s3 = boto3.client('s3')
-processor = AutoImageProcessor.from_pretrained(model_dir)
-model = AutoModelForImageClassification.from_pretrained(model_dir)
+print("Model loaded successfully")
+
+# processor = AutoImageProcessor.from_pretrained(model_dir)
+# model = AutoModelForImageClassification.from_pretrained(model_dir)
+#for local test
+# model_dir = "rsc\model"
+# processor = AutoImageProcessor.from_pretrained(model_dir)
+# model = AutoModelForImageClassification.from_pretrained(model_dir)
+# image = Image.open("test.jpg").convert("RGB")
+# input = processor(images=image, return_tensors="pt")
+# with torch.no_grad():
+#     outputs = model(**input)
+#     logits = outputs.logits
+#     predicted_class_idx = logits.argmax(-1).item()
+#     label = model.config.id2label[predicted_class_idx]
+#     print(label)
 
 def lambda_handler(event, context):
+    print("Received event: ", event)
     record = event['Records'][0]['s3']
     bucket = record['bucket']['name']
     key = record['object']['key']
@@ -19,9 +35,11 @@ def lambda_handler(event, context):
     image_obj = s3.get_object(Bucket=bucket, Key=key)
     image = Image.open(BytesIO(image_obj['Body'].read())).convert("RGB")
     input = processor(images=image, return_tensors="pt")
+    print("Input prepared successfully")
     with torch.no_grad():
         outputs = model(**input)
     logits = outputs.logits
+    print("Model inference completed successfully")
     predicted_class_idx = logits.argmax(-1).item()
     label = model.config.id2label[predicted_class_idx]
 
@@ -31,6 +49,7 @@ def lambda_handler(event, context):
     }
 
     result_key = key.replace("uploads/", "results/").replace(".jpg", ".json")
+    print("Result key: ", result_key)
 
     s3.put_object(
         Bucket=bucket,
